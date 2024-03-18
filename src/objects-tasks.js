f86f27f9f8a89e1029db05457550405c55f9492a
Denis <denisbernovich@gmail.com>
2024-03-18 13:44:25 +0300

    feat: makeWord - done

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -113,8 +113,15 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const arr = [];
+  Object.keys(lettersObject).forEach((key) => {
+    const positions = lettersObject[key];
+    positions.forEach((position) => {
+      arr[position] = key;
+    });
+  });
+  return arr.join('');
 }
 
 /**
